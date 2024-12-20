09761bb89a76d20270352658c0154494885e6aef
Johanu GANDONOU <johanugandonou@gmail.com>
2024-12-20 16:54:56 +0100

    test : test_add_event()

--- test.py
+++ test.py
@@ -1,7 +1,13 @@
 import main
+from datetime import datetime, timedelta
 
 def test_hello():
     assert main.hello() == "Hello World !"
 
 def test_events_type():
     assert isinstance(main.events, list)
+
+def test_add_event():
+    start_time = datetime.now()
+    main.create_event(start_time, 100, "Event 1")
+    assert main.events == [(start_time, 100, "Event 1")]
